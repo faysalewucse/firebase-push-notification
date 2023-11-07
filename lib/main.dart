fd8c56fd144e8f30ddc168a8cf08c749c0f0a224
Zaag <faysal.ewucse@gmail.com>
2023-11-07 18:01:36 +0600

    add notification to foreground

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:firebase_push_notification/audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
@@ -40,8 +39,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   });
 
   VolumeController().setVolume(0.9);
-  print(
-      '================>>>  ${VolumeController().getVolume()}  <<<====================');
+  print('================>>>  ${VolumeController().getVolume()}  <<<====================');
 
   AwesomeNotifications().createNotification(
     content: NotificationContent(
@@ -80,16 +78,16 @@ Future<void> onActionReceivedMethod(ReceivedAction event) async {
     audioPlayer.stop();
     print("second set for reject");
   } else if (event.buttonKeyPressed == "ACCEPT") {
-    VolumeController().setVolume(0.7);
-    final response = await http.get(Uri.parse('https://reqres.in/api/users/2'));
-
-    if (response.statusCode == 200) {
-      debugPrint("==========response: ${response.body}", wrapWidth: 1024);
-      VolumeController().setVolume(mobileCurrentVol);
-    } else {
-      // If the server did not return a 200 OK response, throw an exception
-      throw Exception('Failed to load data');
-    }
+    // VolumeController().setVolume(0.7);
+    // final response = await http.get(Uri.parse('https://reqres.in/api/users/2'));
+    //
+    // if (response.statusCode == 200) {
+    //   debugPrint("==========response: ${response.body}", wrapWidth: 1024);
+    //   VolumeController().setVolume(mobileCurrentVol);
+    // } else {
+    //   // If the server did not return a 200 OK response, throw an exception
+    //   throw Exception('Failed to load data');
+    // }
 
     await audioPlayer.stop();
     // Get.to(()=>StopAudio(audioPlayer: audioPlayer));
@@ -127,6 +125,38 @@ void main() async {
       ],
       debug: true);
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async{
+    await audioPlayer.play(AssetSource('preview.mp3'));
+
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: 101,
+        channelKey: "call_channel",
+        color: Colors.white,
+        title: message.notification!.title,
+        body: message.notification!.body,
+        wakeUpScreen: true,
+        category: NotificationCategory.Message,
+        fullScreenIntent: true,
+        autoDismissible: true,
+        backgroundColor: Colors.orange,
+      ),
+      actionButtons: [
+        NotificationActionButton(
+          key: 'ACCEPT',
+          label: "Accept Call",
+          color: Colors.green,
+          autoDismissible: true,
+        ),
+        NotificationActionButton(
+            key: 'REJECT',
+            label: "Reject Call",
+            color: Colors.redAccent,
+            autoDismissible: true,
+            actionType: ActionType.SilentBackgroundAction),
+      ],
+    );
+  });
   AwesomeNotifications()
       .setListeners(onActionReceivedMethod: onActionReceivedMethod);
   runApp(const MyApp());
@@ -206,13 +236,29 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: GestureDetector(
-          onTap: () => AudioService().stopSound(),
+          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const SecondPage())),
           child: Icon(
             Icons.notifications_active,
             size: 50,
             color: Theme.of(context).colorScheme.inversePrimary,
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("2nd Page"),
+      ),
+      body: const Center(
+        child: Icon(Icons.notification_add),
       ),
     );
   }
